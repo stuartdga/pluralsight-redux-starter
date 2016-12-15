6743cb55f0ff9aa1a736925c6cddee771f6e654f
stuartdga <stuartdga@yahoo.com>
2016-12-15 11:58:41 -0500

    Component for sorting in table headers

---
+++ src/components/common/SortableHeader.js
@@ -0,0 +1,28 @@
+import React, {PropTypes} from 'react';
+
+class SortableHeader extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {direction: 'asc'};
+    this.clickSort = this.clickSort.bind(this);
+  }
+
+  clickSort(event) {
+    this.props.onSort(event);
+    if (this.state.direction == 'asc')
+      this.setState({direction: 'desc'});
+  }
+
+  render() {
+    return <th><a href="#" data-column={this.props.propName} data-direction={this.state.direction} onClick={this.clickSort}>{this.props.propValue}</a></th>;
+  }
+}
+
+SortableHeader.propTypes = {
+  propName: PropTypes.string.isRequired,
+  propValue: PropTypes.string.isRequired,
+  onSort: PropTypes.func.isRequired
+};
+
+export default SortableHeader;
